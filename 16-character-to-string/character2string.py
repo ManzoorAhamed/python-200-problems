fd5eec928d7278c5fc1b9a7b603ece30e3b431a7
Manzoor <manzoor.sr@gmail.com>
2021-10-26 18:43:02 +0530

    character to string 2

--- 16-character-to-string/character2string.py
+++ 16-character-to-string/character2string.py
@@ -4,10 +4,16 @@
 
 ## Solution
 
-list_var=['a', 'b', 'c', 'd']
-str_var=""
+list_var = ['a', 'b', 'c', 'd']
+str_var = ""
 
 for item in list_var:
     str_var=str_var+item
-
 print(str_var)
+
+## Solution2
+
+s = ['a', 'b', 'c', 'd']
+str1 = ''.join(s)   #.join is function which converts list into string
+print(str1)
+
