99981a28867c4443a7eec324cced568e218d3cac
Manzoor <manzoor.sr@gmail.com>
2021-10-27 20:38:41 +0530

    Diff between the lists

---
+++ 19-different-between-2-list/diff-2-list.py
@@ -0,0 +1,17 @@
+# Write a Python program to get the difference between the two lists
+# Input 
+# list1 = [1, 2, 3, 4]
+# list2 = [1, 2]
+# Output
+# [3,4]
+
+## Solution
+list1 = [1, 2, 3, 4]
+list2 = [1, 2]
+diff=[]
+for item in list1:
+    if item not in list2:
+        diff.append(item)
+print(diff)
+
+#print(set(list1) - set(list2))
