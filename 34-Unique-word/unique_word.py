31a28c3e69edb862af14ffde5d550bed60deaf6a
Manzoor <manzoor.sr@gmail.com>
2021-11-04 20:07:06 +0530

    Unique words

---
+++ 34-Unique-word/unique_word.py
@@ -0,0 +1,22 @@
+# Write a Python program that accepts a comma separated sequence of words as input and prints the unique words in sorted form (alphanumerically)
+# Sample Words : red, white, black, red, green, black
+# Expected Result : black, green, red, white,red
+
+## Solution
+random_words = "red, white, black, red, green, black"
+random_words_list = random_words.split(",")
+random_words_list.sort()
+unique_words = []
+for word in random_words_list:
+    if word not in unique_words:
+        unique_words.append(word)
+unique_words_string = " "
+for u_word in unique_words:
+    unique_words_string += u_word+", "
+print(unique_words_string)
+
+## Solution 2
+items = input("Input comma separated sequence of words")
+words = [word for word in items.split(",")]
+print(words)
+print(",".join(sorted(list(set(words)))))
