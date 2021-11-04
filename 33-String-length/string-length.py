10fd66ca7d8738eaf7b535aa5a296e359d2eeb34
Manzoor <manzoor.sr@gmail.com>
2021-11-04 19:36:56 +0530

    string length

---
+++ 33-String-length/string-length.py
@@ -0,0 +1,13 @@
+# Write a Python program to calculate the length of a string. 
+
+## Solution
+string = input("Enter the string : ")
+print(len(string))
+
+## Solution2
+def string_length(str1):
+    count = 0
+    for char in str1:
+        count += 1
+    return count
+print(string_length('hello world'))
