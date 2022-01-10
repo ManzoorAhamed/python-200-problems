dbf6cefa99354213e500592a540d6f44dee26d6e
Manzoor <manzoor.sr@gmail.com>
2022-01-10 19:22:54 +0530

    Added the solution 2

--- 36-chk-key-exist/chk-key-exist.py
+++ 36-chk-key-exist/chk-key-exist.py
@@ -7,8 +7,7 @@
 # Key is present in the dictionary                                                                              
 # Key is not present in the dictionary
 
-
-
+## Solution
 def is_key_present(n):
     d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
     key = d.keys()
@@ -17,5 +16,15 @@ def is_key_present(n):
     else:
         print("Key is not present in the dict")
 
+is_key_present(5)
+is_key_present(9)
+
+#### Solution 2
+d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+def is_key_present(x):
+  if x in d:
+      print('Key is present in the dictionary')
+  else:
+      print('Key is not present in the dictionary')
 is_key_present(5)
 is_key_present(9)
