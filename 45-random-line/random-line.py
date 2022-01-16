5c215818e82a788d29295fc83a864723e501245a
Manzoor <manzoor.sr@gmail.com>
2022-01-16 12:41:45 +0530

    Reading Random line from tect file

--- 45-random-line/random-line.py
+++ 45-random-line/random-line.py
@@ -7,5 +7,7 @@ import random
 def random_line(afile):
     f = open(afile).read().splitlines()
     return random.choice(f)
-
 print(random_line("/home/arhan/VScode/python-200-problems/44-longest-word/test.txt"))
+
+## Reference : https://www.w3resource.com/python-exercises/file/python-io-exercise-15.php
+## Replace the path according to your systemPath
